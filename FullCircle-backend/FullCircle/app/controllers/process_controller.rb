f8e7838a1b885a8fa7f55c9ca40f2cb696d843ca
lukecrum20 <lukecrum20@gmail.com>
2018-05-07 09:31:03 -0400

    db upload

--- FullCircle-backend/FullCircle/app/controllers/process_controller.rb
+++ FullCircle-backend/FullCircle/app/controllers/process_controller.rb
@@ -1,10 +1,16 @@
 class ProcessController < ApplicationController
   def process_resource
     @resource = Resource.new
-    @resource.filename = File.basename(params[:file])
+    # Instance of UploadedFile classes
+    # http://api.rubyonrails.org/classes/ActionDispatch/Http/UploadedFile.html#method-i-read
+    uploaded_file = params["Resource"][:file]
+
+    # render plain: uploaded_file.original_filename
+    @resource.filename = uploaded_file.original_filename
+    # ETC...
     @resource.type_of_resource = params[:type_of_resource]
-    @resource.data = params[:file].read
-    @resource.content_type = params[:file].content_type
+    @resource.data = uploaded_file.read
+    @resource.content_type = uploaded_file.content_type
     @resource.save
   end
 end
