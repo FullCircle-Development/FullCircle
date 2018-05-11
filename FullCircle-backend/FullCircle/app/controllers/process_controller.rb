8d7cee63f342cf6df55217c4ba24fbf0355fb39e
lukecrum20 <lukecrum20@gmail.com>
2018-05-11 10:33:20 -0400

    add descriptions to each page

--- FullCircle-backend/FullCircle/app/controllers/process_controller.rb
+++ FullCircle-backend/FullCircle/app/controllers/process_controller.rb
@@ -1,13 +1,9 @@
 class ProcessController < ApplicationController
   def process_resource
     @resource = Resource.new
-    # Instance of UploadedFile classes
-    # http://api.rubyonrails.org/classes/ActionDispatch/Http/UploadedFile.html#method-i-read
     uploaded_file = params["Resource"][:file]
     description = params[:description]
-    # render plain: uploaded_file.original_filename
     @resource.filename = uploaded_file.original_filename
-    # ETC...
     @resource.type_of_resource = params[:type_of_resource]
     @resource.data = uploaded_file.read
     @resource.content_type = uploaded_file.content_type
