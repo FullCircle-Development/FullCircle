d2385cc7010df24351104a5fee8239439041400f
lukecrum20 <lukecrum20@gmail.com>
2018-06-16 00:01:51 -0400

    basically finished the site

--- FullCircle-backend/FullCircle/config/initializers/assets.rb
+++ FullCircle-backend/FullCircle/config/initializers/assets.rb
@@ -13,6 +13,8 @@ Rails.application.config.assets.precompile += %w( upload.css )
 Rails.application.config.assets.precompile += %w( application.css )
 
 Rails.application.config.assets.precompile += %w( about.css )
+Rails.application.config.assets.precompile += %w( bootstrap2.css )
+Rails.application.config.assets.precompile += %w( 1-col-portfolio.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
--- FullCircle-backend/FullCircle/config/routes.rb
+++ FullCircle-backend/FullCircle/config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  match '/programming' => 'programming#programming', via: :get
-  match '/awards' => 'awards#awards', via: :get
-  match '/electrical' => 'electrical#electrical', via: :get
-  match '/fundraising' => 'fundraising#fundraising', via: :get
-  match '/mechanical' => 'mechanical#mechanical', via: :get
-  match '/outreach' => 'outreach#outreach', via: :get
+
+  match '/technical' => 'programming#programming', via: :get
+  match '/team_building' => 'awards#awards', via: :get
   match '/about_us' => 'about#about', via: :get
+  match '/home' => 'home#home', via: :get
 
   post '/process' => 'process#process_resource'
   post '/download' => 'download#download_file'
-  match '/upload' => 'upload#upload', via: :get
+  match '/upload-155893305789872erwoiu54oiujk345' => 'upload#upload', via: :get
 
   get '/filter/:type' => 'download#find_by'
 end
